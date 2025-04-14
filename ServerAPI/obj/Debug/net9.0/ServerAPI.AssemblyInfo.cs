89a2fd39da9357106e6e9091e26e08d2b7e0e7b7
Goncalo Moreira <goncalomoreiragit99@gmail.com>
2025-04-14 14:40:05 +0100

    tutor application  14/04/2025

--- ServerAPI/obj/Debug/net9.0/ServerAPI.AssemblyInfo.cs
+++ ServerAPI/obj/Debug/net9.0/ServerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283cfceaa5e7823d908eb20c73bb912b32c793d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d40971455958fe8a22dcaaafdb9fcf3b1be0efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
